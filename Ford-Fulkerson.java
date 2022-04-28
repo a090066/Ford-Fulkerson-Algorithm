4fc009c930a7ace7c592f972c83b42760ffc0ecd
JaeilAn <a090066@gmail.com>
2022-04-29 08:58:49 +0900

    Report

---
+++ Ford-Fulkerson.java
@@ -0,0 +1,59 @@
+## Ford-Fulkerson 코드 구현
+
+```java
+public static int capacity[][];
+    public static int flow[][];
+    public static int path[]; // 
+    public static boolean visited[];
+    public static LinkedList<Integer> graph[];
+
+    public static boolean dfs(int start) {
+        if (start == Sink) {
+            return true;
+        }
+
+        visited[start] = true;
+        LinkedList<Integer> nexts = graph[start];
+        for (int next: nexts) {
+            if ( !visited[next] && capacity[start][next] - flow[start][next] > 0) {
+                path[next] = start;
+
+                if (dfs(next)) { // DFS(깊이 우선 탐색) 방식으로 계속 경로를 찾고, 찾으면 탈출
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // O((V+E)F)
+    public static int FordFulkerson() {
+        int total = 0;
+        while (dfs(Source)) { // dfs로 경로 찾기(증가경로), 경로가 더이상 없으면 종료임.
+            // 경로에서 흘릴수 있는 최대의 유량(flow)을 찾기
+            int flowNum = Integer.MAX_VALUE;
+            for(int i = Sink; i != Source; i = path[i]) {
+                int from = path[i];
+                int to = i;
+                flowNum = Math.min(flowNum, (capacity[from][to]) - flow[from][to]);
+            }
+            // 찾은 경로에 유량을 흘려보내기. 역방향도 흘려주기.
+            for(int i = Sink; i != Source; i = path[i]) {
+                int from = path[i];
+                int to = i;
+
+                flow[from][to] += flowNum;
+                flow[to][from] -= flowNum;
+            }
+
+            total += flowNum;
+
+            // 찾은 경로를 초기화 한다. 
+            // 그리고 다시 dfs로 s(Source) 에서 t(Sink) 까지 경로를 찾는다.
+            Arrays.fill(path, -1);
+            Arrays.fill(visited, false);
+        }
+        return total;
+    }
+
+```
